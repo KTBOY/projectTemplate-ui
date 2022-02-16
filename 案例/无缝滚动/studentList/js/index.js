9dd95b2fc4e014ead2982f9de2971e72101cbe88
Zlc <Zlc@123.com>
2022-02-16 19:10:34 +0800

    fix:完善虚拟长列表

--- 案例/无缝滚动/studentList/js/index.js
+++ 案例/无缝滚动/studentList/js/index.js
@@ -1,5 +1,6 @@
 	(function($) {
 		var box = document.getElementById("bm_content");
+		
 		var l1 = document.getElementById("tb1");
 		var l2 = document.getElementById("tb2");
 		var state = {
@@ -24,24 +25,23 @@
 
 		function autoScroll() {
 			var product = RenderList();
+			console.log(box)
 			l1.innerHTML = product;
 			if (l1.offsetHeight > box.offsetHeight) {
 				l2.innerHTML = l1.innerHTML; //克隆list1的数据，使得list2和list1的数据一样
-				scrollMove = setInterval(scrollup, 50); //数值越大，滚动速度越慢
+				scrollMove = setInterval(scrollup, 35); //数值越大，滚动速度越慢
 				box.onmouseover = function() {
-					// clearInterval(scrollMove)
+					 clearInterval(scrollMove)
 				}
 			}
 		}
 
 		function scrollup() {
+			
 			//滚动条距离顶部的值恰好等于list1的高度时，达到滚动临界点，此时将让scrollTop=0,让list1回到初始位置，实现无缝滚动
 			if (box.scrollTop >= l1.offsetHeight) {
 				box.scrollTop = 0;
-				if (page.pageNo >= totalPage) {
-					
-					return
-				}
+				if (page.pageNo >= totalPage) return
 				page.pageNo++;
 				getList();
 			} else {
@@ -51,7 +51,7 @@
 		}
 		//鼠标离开时，滚动继续
 		box.onmouseout = function() {
-			//scrollMove = setInterval(scrollup, 30);
+			scrollMove = setInterval(scrollup, 30);
 		}
 
 		function RenderList() {
